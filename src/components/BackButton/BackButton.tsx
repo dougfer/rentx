544b14ef3b433318b9fa47738fbf6b3a4b1134a2
Douglas Ferreira <douglas.ferreira@mutual.club>
2022-08-06 18:41:53 -0300

    feat: creating car details view

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { Container } from './styles'
 import { MaterialIcons } from '@expo/vector-icons'
 import { useTheme } from 'styled-components'
+import { BorderlessButtonProps } from 'react-native-gesture-handler'
 
-interface BackButtonProps {
+interface BackButtonProps extends BorderlessButtonProps {
   color?: string
 }
 
-export const BackButton: React.FC<BackButtonProps> = ({ color }) => {
+export const BackButton: React.FC<BackButtonProps> = ({ color, ...rest }) => {
 
   const theme = useTheme()
 
  return (
-   <Container>
+   <Container {...rest}>
     <MaterialIcons 
       name='chevron-left'
       size={24}
       color={color || theme.colors.text}
     />
-
    </Container>
  )
 }
--- src/screens/CarDetails/CarDetails.tsx
+++ src/screens/CarDetails/CarDetails.tsx
@@ -1,13 +1,24 @@
 
 import React from 'react'
-import { Container, Header } from './styles'
+import { Container, Header, CarImages } from './styles'
+import { BackButton, ImageSlider } from 'src/components'
+import { StatusBar } from 'react-native'
 
 export const CarDetails: React.FC = () => {
  return (
-   <Container>
-      <Header>
-
-      </Header>
-   </Container>
+    <>
+      <StatusBar
+        barStyle='dark-content' 
+        backgroundColor='transparent'
+      />
+      <Container>
+        <Header>
+          <BackButton onPress={() => {}} />
+        </Header>
+        <CarImages>
+          <ImageSlider imagesUrl={['https://img1.gratispng.com/20171220/kiq/audi-png-car-image-5a3b1f1eb47de9.9104985015138240307393.jpg']} />
+        </CarImages>
+      </Container>
+    </>
  )
 }
